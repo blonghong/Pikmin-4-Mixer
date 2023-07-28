41aa64f9f5ea0ae9ebfdd8772bec4924dae5cab9
blonghong <jamiealexhess@gmail.com>
2023-07-28 15:39:13 +0800

    rename & gitignore

--- Pikmin4SoundSorter/My Project/Settings.Designer.vb
+++ Pikmin4SoundSorter/My Project/Settings.Designer.vb
@@ -76,9 +76,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.WAVSorterForPikmin4.My.MySettings
+        Friend ReadOnly Property Settings() As Global.Pikmin4SoundSorter.My.MySettings
             Get
-                Return Global.WAVSorterForPikmin4.My.MySettings.Default
+                Return Global.Pikmin4SoundSorter.My.MySettings.Default
             End Get
         End Property
     End Module
